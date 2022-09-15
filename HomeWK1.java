d9272e211bfc877bd5a29cd2243e5239f97718bb
banyaadski <89918249+banyaadski@users.noreply.github.com>
2022-09-15 15:19:23 +0300

    Add files via upload
    2.0

---
+++ HomeWK1.java
@@ -0,0 +1,38 @@
+import java.io.FileNotFoundException;
+
+public class HomeWK1 {
+    public static void task2() {
+        int[] intArray = new int[10];
+        try {
+            int d = 0;
+            double catchedRes1 = intArray[8] / d;
+            System.out.println("catchedRes1 = " + catchedRes1);
+        } catch (ArithmeticException e) {
+            System.out.println("Catching exception: " + e);
+        }
+        System.out.println("Ошибка поймана");
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        try {
+            int a = 90;
+            int b = 3;
+            System.out.println(a / b);
+            printSum(23, 234);
+            int[] abc = { 1, 2 };
+            abc[3] = 9;
+
+        } catch (NullPointerException ex) {
+            System.out.println("Указатель не может указывать на null!");
+        } catch (IndexOutOfBoundsException ex) {
+            System.out.println("Массив выходит за пределы своего размера!");
+        } catch (Exception ex) {
+            System.out.println("Что-то пошло не так...");
+        }
+    }
+
+    public static void printSum(Integer a, Integer b) throws FileNotFoundException {
+        System.out.println(a + b);
+    }
+}
